df80cee88b011340d41b42c61a1903cfdef02dd3
egorw <egorw@message.com>
2023-01-04 13:39:43 +0300

    Добавлена функция для проверки результата.

--- samples/OkoloIt.Utilities.Result.Samples/Program.cs
+++ samples/OkoloIt.Utilities.Result.Samples/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using OkoloIt.Utilities.Result;
+
+static IResult<int> GetPositiveNumber(int value)
+{
+    if (value < 0)
+        return Result.Fail<int>("Число отрицательное!");
+
+    return Result.Ok(value);
+}
+
+IResult<int> number = GetPositiveNumber(-5);
+
+if (number.Success)
+    Console.WriteLine($"Число: {number.Value}");
+else
+    Console.WriteLine(number.Message);
