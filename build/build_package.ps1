54f0ad96d9324b17f0fdcab77c73fcbb6c861d3d
egorw <egorw@message.com>
2023-01-04 13:32:02 +0300

    Добавлены скрипты для сборки пакета.

---
+++ build/build_package.ps1
@@ -0,0 +1,9 @@
+﻿# Clear Release folder
+$packagePath = $sourceDirectory + "\bin\Release\*.nupkg";
+
+Remove-Item -Path $packagePath;
+dotnet clean $csProjfileName -c Release;
+
+# Build Project
+dotnet build $csProjfileName -c Release;
+dotnet pack  $csProjfileName -c Release;
---
+++ build/build_package_new_version.ps1
@@ -0,0 +1,23 @@
+﻿# Update version
+$sourceDirectory = "..\src\OkoloIt.Utilities.Result";
+$csProjfileName = $sourceDirectory + "\OkoloIt.Utilities.Result.csproj";
+
+[xml]$projectContents = Get-Content -Path $csProjfileName;
+$oldVersion = [Version]$projectContents.Project.PropertyGroup.Version;
+$newVersion = [Version]::new($oldVersion.Major ,$oldVersion.Minor ,$oldVersion.Build + 1);
+
+# Save new version
+$projectContents.Project.PropertyGroup.Version = $newVersion.ToString();
+$projectContents.Save($csProjfileName);
+
+"Version number has been udated from " + $oldVersion + " to " + $newVersion;
+
+# Clear Release folder
+$packagePath = $sourceDirectory + "\bin\Release\*.nupkg";
+
+Remove-Item -Path $packagePath;
+dotnet clean $csProjfileName -c Release;
+
+# Build Project
+dotnet build $csProjfileName -c Release;
+dotnet pack  $csProjfileName -c Release;
