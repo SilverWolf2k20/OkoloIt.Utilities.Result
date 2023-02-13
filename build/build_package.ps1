add386bcc720f421b7bb3e5cbdb08c56fc8471b3
egorw <egorw@DESKTOP-TCDMSCK>
2023-02-14 01:36:54 +0300

    feat: изменен путь к файлу проекта

--- build/build_package.ps1
+++ build/build_package.ps1
@@ -1,9 +1,9 @@
 ﻿# Taken from psake https://github.com/psake/psake
 
 # Clear Release folder
-Remove-Item -Path ..\src\OkoloIt.Utilities.Result\bin\Release\*.nupkg
-dotnet clean ..\src\OkoloIt.Utilities.Result\OkoloIt.Utilities.Result.csproj -c Release;
+Remove-Item -Path ..\src\bin\Release\*.nupkg
+dotnet clean ..\src\OkoloIt.Utilities.Result.csproj -c Release;
 
 # Build Project
-dotnet build ..\src\OkoloIt.Utilities.Result\OkoloIt.Utilities.Result.csproj -c Release;
-dotnet pack ..\src\OkoloIt.Utilities.Result\OkoloIt.Utilities.Result.csproj -c Release;
+dotnet build ..\src\OkoloIt.Utilities.Result.csproj -c Release;
+dotnet pack ..\src\OkoloIt.Utilities.Result.csproj -c Release;
